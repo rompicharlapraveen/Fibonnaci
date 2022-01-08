9bb819deb3bbcccf5a2b6da4c340c77c4643f9c3
praveen <rompicharlapraveen29@gmail.com>
2022-01-08 12:14:36 +0530

    programs

---
+++ Practice Programs/Fibanocci.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Practice_Programs
+{
+    class Fibanocci
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 0, n2 = 1, n3, i, a;
+            Console.WriteLine("Enter the element: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write(n1 + " " + n2 + " ");    
+            for (i = 0; i < a; i++)   
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Practice Programs/StringRev.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Practice_Programs
+{
+    class StringRev
+    {
+        public static void Main(string[] args)
+        {
+            int l;
+            string str1, str2 = "";
+            Console.WriteLine("Enter Your String");
+            str1 = Console.ReadLine();
+            for(l=str1.Length-1;l>=0;l--)
+            {
+                str2 = str2 + str1[l];
+            }
+            Console.WriteLine(str2);
+            Console.ReadLine();
+        }
+    }
+}
